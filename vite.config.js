53a69cd85294171b3fa7d0cfcf0b4c535702cf9e
Semen Belov <cool.gsmes@yandex.ru>
2024-05-14 12:23:58 +0300

    Поправил сборку

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { resolve } from 'path';
-import {createSvgIconsPlugin} from "vite-plugin-svg-icons";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
-export default ({
+export default {
     root: resolve(__dirname, 'public'),
+    base: './',
+    build: {
+        outDir: resolve(__dirname, 'docs'),
+    },
     plugins: [
         createSvgIconsPlugin({
             iconDirs: [resolve(process.cwd(), 'public/assets/icons/')],
             symbolId: 'icon-[name]',
         }),
     ],
-});
+};
